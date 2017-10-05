701c7a35cf4d1f85270854b829c00d31456cd819
Prundus Daniel <prundus_daniel_fa@yahoo.com>
2017-10-05 12:01:38 +0300

    add references by name  and also add support for post picture and text

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from selenium.common.exceptions import NoSuchElementException
 import ConfigParser
 import sys
 
-driver = webdriver.PhantomJS()
+driver = webdriver.Chrome()
 driver.get('https://mbasic.facebook.com')
 Config = ConfigParser.ConfigParser()
 Config.read("config.ini")
@@ -29,13 +29,15 @@ def ConfigSectionMap(section):
 data = ConfigSectionMap("FBData")
 username = data['username']
 password = sys.argv[1]
+#text = 'Angajam animator/supraveghetor evenimente/loc de joaca copii !\nLocatie: TheMagic House: str. C-tin Brancusi nr 1 -la etaj- Cluj-Napoca\nDetalii la telefon: 0756 977 516\nCerinte:\n- persoana sociabila, comunicativa, dornica sa fie in preajma copiilor si sa se joace cu ei;\n- indemanatica si creativa;\n- spirit de echipa si vesela;\n- disponibilitate in weekend.\nResponsabilitati:\n- supravegherea si entertainment-ul/animarea copiilor in spatiul de joaca in timpul activitatilor/evenimentului;\n- organizarea si amenajarea si spatiului de joaca atat pentru evenimente cat si pentru joaca si ateliere;\n- pastrarea si depozitarea jucariilor pentru o desfasurare a activitatilor in conditii cat mai bune in spatiul de joaca;\n- pregatirea si servirea mesei pentru evenimente;\n- pastrarea ordinii si curateniei;\n- buna dispozitie si veselie.\nVa rugam sa trimiteti CV-urile la adresa de mail:\nThemagichousecj@gmail.com\n Fa un copil sa zambeasca cu zambetul tau si te luam in echipa de la TheMagic House Cluj Napoca!\n'
+#print text
 groupData=""
 print "Opened facebook..."
 driver.find_element_by_id('m_login_email').send_keys(username)
 print "Email Id entered..."
 driver.find_element_by_name('pass').send_keys(password)
 print "Password entered..."
-driver.find_element_by_xpath('//*[@id="login_form"]/ul/li[3]/input').click() #login button
+driver.find_element_by_name('login').click() #login button
 sleep(2);
 driver.find_element_by_xpath('//*[@id="root"]/table/tbody/tr/td/div/a').click() #ignore one login
 sleep(2);
@@ -47,24 +49,30 @@ with open('groups.txt') as fp:
         sleep(2)
         driver.get(groupURL);#casuta jucariilor
         print "Go to group"
-        driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[3]/form/div/span/div[1]/table/tbody/tr/td[2]/input').click() #click on post picture
+        #driver.find_element_by_name("xc_message").send_keys(text)
+        #driver.save_screenshot('text.png')
+        #print "Add Text"
+        #driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[3]/form/div/span/div[1]/table/tbody/tr/td[2]/input').click() #click on post picture
+        driver.find_element_by_name("view_photo").click()
+        driver.save_screenshot('group.png')
         print "click on post picture"
         sleep(2)
-        driver.find_element_by_xpath('//*[@id="root"]/table/tbody/tr/td/form/div[1]/div/input').send_keys("/Users/dprundus/PycharmProjects/facebook/om zap.jpeg")
-        #driver.find_element_by_name('file1').send_keys(groupPicture)
+        driver.find_element_by_name("file1").send_keys(groupPicture)
         driver.save_screenshot('picture.png')
         print "Choose file"
 
         sleep(2)
-        driver.find_element_by_xpath('//*[@id="root"]/table/tbody/tr/td/form/div[3]/input[1]').click() #click previzualizare
+        driver.find_element_by_name('add_photo_done').click() #click previzualizare
         print "click on prev"
         driver.save_screenshot('screenshot1.png')
         sleep(2)
-        driver.find_element_by_xpath('//*[@id="composer_form"]/input[17]').click() #click POsteaza
+        driver.find_element_by_name('view_post').click() #click POsteaza
         sleep(2)
         driver.save_screenshot('screenshot2.png')
-        driver.find_element_by_xpath('//*[@id="root"]/table/tbody/tr/td/form/div[5]/input').click() #click POsteaza
+        driver.find_element_by_name('done').click() #click POsteaza
 driver.close();
 #driver.get('https://mbasic.facebook.com/prundus.d.ioan')
-#driver.find_element_by_name("xc_message").send_keys('test daniel, ignorati')
+
+
 #driver.find_element_by_name("view_post").click()
+#driver.find_element_by_name("view_photo").click()
